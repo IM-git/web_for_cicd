36f42067d0a7f8f6e6e25b5575421696e341410a
IM94 <gladiator.pw@gmail.com>
2024-03-10 21:25:46 +0300

    Added paths: about, user, contact

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,26 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Test Web-Project for the CI/CD.")
+    host = request.META["HTTP_HOST"]  # get the server address
+    user_agent = request.META["HTTP_USER_AGENT"]  # getting browser data
+    path = request.path  # getting the requested path
+    return HttpResponse(f"""<h1>Test Web-Project for the CI/CD.</h1>
+                            <p>Host: {host}</p>
+                            <p>User agent: {user_agent}</p>
+                            <p>Path: {path}</p>""")
+
+
+def about(request, name, age):
+    return HttpResponse(f"""
+                            <h1>About:</h1>
+                            <p>Name: {name}</p>
+                            <p>Age: {age}</p>
+                        """)
+
+
+def contact(request):
+    return HttpResponse("<h1>Contacts</h1>")
+
+
+def user(request, name="Undefined", age=666):
+    return HttpResponse(f"<h2>Name: {name}, Age: {age}</h2>")
--- web_for_cicd/urls.py
+++ web_for_cicd/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path,re_path
 from hello import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('about/contacts', views.contact, name='contacts'),
+    re_path(r'^about', views.about, kwargs={"name": "IM", "age": 30}),
+    path('user', views.user),
+    path('user/<str:name>', views.user),
+    path('user/<str:name>/<int:age>', views.user),
+    path('', views.index, name='index')
 ]
